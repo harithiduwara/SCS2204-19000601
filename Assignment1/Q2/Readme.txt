f9b0be9b4fd61644d5a538c327573cf22dd1defb
harithiduwara <84234311+harithiduwara@users.noreply.github.com>
2021-05-16 22:03:32 +0530

    Question 02
    2.The volume of a sphere with radius r is 4/3 Pi r3. What is the volume of a sphere with radius 5?

---
+++ Assignment1/Q2/Readme.txt
@@ -0,0 +1,1 @@
+2.The volume of a sphere with radius r is 4/3 Pi r3. What is the volume of a sphere with radius 5?
---
+++ Assignment1/Q2/Volumeofsphere.scala
@@ -0,0 +1,7 @@
+object volumeofsphere extends App{
+  def vol(X:Double)=X*X*X*4*22/21;
+
+  println("Volume of a sphere with radius of 5 = ");
+  println(vol(5));
+
+}
